de35305221dd7cb3d5f4766b3b95c519d2f9f45f
TD <denningta@gmail.com>
2020-01-10 22:18:00 -0700

    Add ajax to search

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,14 @@
+class StocksController < ApplicationController
+
+    def search
+        if params[:stock].blank?
+            flash.now[:danger] = "You must enter a stock ticker symbol"
+        else
+            @stock = Stock.new_from_lookup(params[:stock])
+            flash.now[:danger] = "You have entered and incorrect symbol" unless @stock
+        end
+        respond_to do |format|
+            format.js { render partial: 'users/result' }
+        end
+    end
+end
